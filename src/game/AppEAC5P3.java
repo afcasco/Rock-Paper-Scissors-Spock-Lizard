e451d8c0dfc69ef7f040d614879b25896cc77d02
afcasco <afcasco@gmail.com>
2022-06-29 00:47:46 +0200

    translated a few methods to english

--- src/game/AppEAC5P3.java
+++ src/game/AppEAC5P3.java
@@ -2,6 +2,8 @@ package game;
 
 public class AppEAC5P3 {
 
+    //testing fladñkjf
+
     private static final String PUNTUACIO_INICIAL = "0";
     private static final String NO_MORE_SPACE_ERROR = """
                          NO QUEDA ESPAI PER REGISTRAR MES JUGADORS
